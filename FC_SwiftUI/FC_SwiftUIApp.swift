6c66620c6b77378af55fb4a92405de740c9db28d
chulyeon <guinness987@gmail.com>
2023-10-15 14:35:06 +0900

    Initial Commit

---
+++ FC_SwiftUI/FC_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FC_SwiftUIApp.swift
+//  FC_SwiftUI
+//
+//  Created by chulyeon kim on 10/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FC_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
